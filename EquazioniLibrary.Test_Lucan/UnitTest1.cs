29c95013988ca6f0003ff1eb60c95f9b02277d06
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-14 09:19:10 +0100

    Refactor metodo IsDetermined

--- EquazioniLibrary.Test_Lucan/UnitTest1.cs
+++ EquazioniLibrary.Test_Lucan/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace EquazioniLibrary.Test_Lucan
     [TestClass]
     public class UnitTest1
     {
-        //Controlli per un'equazione determinata
+        //Controlli per verificare se un'equazione è determinata
         [TestMethod]
         public void Test_IsDetermined()
         {
@@ -40,7 +40,7 @@ namespace EquazioniLibrary.Test_Lucan
             Assert.AreEqual(Risultato, Risultato2);
         }
 
-        //Controlli per un'equazione impossibile
+        //Controlli per verificare se un'equazione è impossibile
         [TestMethod]
         public void Test_IsInconsisted()
         {
@@ -77,7 +77,43 @@ namespace EquazioniLibrary.Test_Lucan
             Assert.AreEqual(Risultato, Risultato2);
         }
 
-        //Controlli per un'equazione di secondo grado
+        //Controlli per verificare se un'equazione è indeterminata
+        public void Test_IsIndetermined()
+        {
+            double b = 0;
+            double a = 0;
+            bool Risultato = true;
+
+            bool Risultato2 = Equazioni.IsIndetermined(a, b);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        [TestMethod]
+        public void Test_IsIndetermined2()
+        {
+            double b = 1;
+            double a = 0;
+            bool Risultato = false;
+
+            bool Risultato2 = Equazioni.IsIndetermined(a, b);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        [TestMethod]
+        public void Test_IsIndetermined3()
+        {
+            double b = -1;
+            double a = 1;
+            bool Risultato = false;
+
+            bool Risultato2 = Equazioni.IsIndetermined(a, b);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        //Controlli per verificare se un'equazione è di secondo grado
         [TestMethod]
         public void Test_IsDegree2()
         {
@@ -107,6 +143,83 @@ namespace EquazioniLibrary.Test_Lucan
             bool Risultato = true;
 
             bool Risultato2 = Equazioni.IsDetermined(a);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        //Contrtolli per il delta
+        [TestMethod]
+        public void Double()
+        {
+            double a = 1;
+            double b = 1;
+            double c = 1;
+            double Risultato = -3;
+
+            double Risultato2 = Equazioni.Delta(a, b, c);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        [TestMethod]
+        public void Double1()
+        {
+            double a = 1;
+            double b = 1;
+            double c = 0;
+            double Risultato = 1;
+
+            double Risultato2 = Equazioni.Delta(a, b, c);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        [TestMethod]
+        public void Double2()
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            double Risultato = 0;
+
+            double Risultato2 = Equazioni.Delta(a, b, c);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        //Controlli per verificare se un'equazione è di 1 grado
+        [TestMethod]
+        public void EquationDegree1()
+        {
+            double a = 0;
+            double b = 0;
+            string Risultato = "L'equazione è indeterminata";
+
+            string Risultato2 = Equazioni.EquationDegree1(a, b);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        [TestMethod]
+        public void EquationDegree1_2()
+        {
+            double a = 1;
+            double b = 0;
+            string Risultato = "L'equazione è impossibile";
+
+            string Risultato2 = Equazioni.EquationDegree1(a, b);
+
+            Assert.AreEqual(Risultato, Risultato2);
+        }
+
+        [TestMethod]
+        public void EquationDegree1_3()
+        {
+            double a = 6;
+            double b = 2;
+            string Risultato = "3";
+
+            string Risultato2 = Equazioni.EquationDegree1(a, b);
 
             Assert.AreEqual(Risultato, Risultato2);
         }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -8,7 +8,7 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
-        //Metodo per un'equazione determinata
+        //Metodo per un'equazione determinata(1)
         public static bool IsDetermined (double a)
         {
             bool Risposta = false;
@@ -18,17 +18,12 @@ namespace EquazioniLibrary
                 Risposta = true;
             }
 
-            else
-            {
-                Risposta = false;
-            }
-
-
+            
 
             return Risposta;
         }
 
-        //Metodo per un'equazione impossibile
+        //Metodo per verificare se un'equazione è impossibile(2)
         public static bool IsInconsisted (double a, double b)
         {
             bool Risposta = false;
@@ -47,7 +42,26 @@ namespace EquazioniLibrary
             return Risposta;
         }
 
-        //Metodo per un'equazione di secondo grado
+        //Metodo per un'equazione impossibile(2.2)
+        public static bool IsIndetermined(double a, double b)
+        {
+            bool Risposta = false;
+
+            if (a == 0 && b == 0)
+            {
+                Risposta = true;
+
+            }
+
+            else
+            {
+                Risposta = false;
+            }
+
+            return Risposta;
+        }
+
+        //Metodo per verificare se un'equazione è di secondo grado(3)
         public static bool IsDegree2 (double a)
         {
             bool Risposta = false;
@@ -64,6 +78,43 @@ namespace EquazioniLibrary
 
 
             return Risposta;
+        }
+
+        //Metodo per calcolare il delta di un'equazione di 2 grado(4)
+        public static double Delta (double a, double b, double c )
+        {
+            double Delta = 0;
+
+            Delta = (b * b) - 4 * a * c;
+
+            return Delta; 
+
+        }
+
+        //Metodo per risolvere un'equazione di 1 grado
+        public static string EquationDegree1 (double a, double b)
+        {
+            string Messaggio = "";
+            double Calcolo = 0;
+
+            if (IsIndetermined(a,b))
+            {
+                Messaggio = "L'equazione è indeterminata";
+            }
+
+            else if (IsInconsisted(a,b))
+            {
+                Messaggio = "L'equazione è impossibile";
+            }
+
+            else
+            {
+                Calcolo = (b / a); 
+            }
+
+            Messaggio = Convert.ToString(Calcolo);
+            
+            return Messaggio;
         }
     }
 }
